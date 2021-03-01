1f7aec0b9c43a991ac939fe4bacdc9ed73e5d6a6
asadasrar <34654001+asadasrar@users.noreply.github.com>
2021-03-01 15:21:51 +0500

    Update Web_Automation.js

--- Web_Automation.js
+++ Web_Automation.js
@@ -1,11 +1,11 @@
 describe('NoCommerce', function () {
     it('Verify Title', function () {
         cy.visit('https://www.nopcommerce.com/en/demo')
-        cy.title().should('eq', 'Store Demo - nopCommerce')
+        cy.title().should('equal', 'Store Demo - nopCommerce')
     })
 
     it('Verify Title-NegativeTest', function () {
         cy.visit('https://www.nopcommerce.com/en/demo')
-        cy.title().should('eq', 'Sign In FraudLens Dental Payment Integrity')
+        cy.title().should('equal', 'Sign In FraudLens Dental Payment Integrity')
     })
 })
